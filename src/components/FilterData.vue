ff4d4a304f20235cfdc7688639af506b5b189720
BIEMAX <tecnico.beilke@gmail.com>
2023-02-05 11:05:12 -0300

    Added new components

---
+++ src/components/FilterData.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>tested</p>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup () {
+    return { }
+  }
+})
+</script>
