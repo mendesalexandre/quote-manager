cf4ed477ccf132a3e9b984f718420b5032c65c07
BIEMAX <tecnico.beilke@gmail.com>
2023-04-02 15:23:21 -0300

    Updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -107,5 +107,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-components': 'off'
   }
 }
