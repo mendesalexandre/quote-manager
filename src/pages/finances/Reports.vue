d8a63b413c125ceaa278e54b577ffd16681c6ba6
BIEMAX <tecnico.beilke@gmail.com>
2023-05-13 05:13:18 -0300

    Added charts for reports

--- src/pages/finances/Reports.vue
+++ src/pages/finances/Reports.vue
@@ -1,106 +1,131 @@
+<!-- eslint-disable semi -->
 <template>
-  <!-- <Pie :data="moneyPerYear" :options="options"/> -->
-  <!-- <Pie :data="moneyPerYearData" :options="options" /> -->
-  <!-- <Pie :data="data" :options="options" /> -->
-  <canvas ref="chart"></canvas>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <div>
+      <q-card>
+        <doc-card-title title="Chart 1" />
+        <Pie v-if="moneyPerYear" :data="moneyPerYear" :options="options" id="1"/>
+      </q-card>
+    </div>
+    <div>
+      <q-card>
+        <doc-card-title title="Chart 2" />
+        <Pie v-if="expensesByYear" :data="expensesByYear" :options="options" id="2"/>
+      </q-card>
+    </div>
+    <div>
+      <q-card>
+        <doc-card-title title="Chart 3" />
+        <Pie v-if="earningsByYear" :data="earningsByYear" :options="options" id="3"/>
+      </q-card>
+    </div>
+  </div>
+  <!-- Finances tips
+  <q-card>
+    <doc-card-title :title="$t('view.home.lbl.sectionOne')" />
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-intersection
+        v-for="(tip, index) in tipsFinances"
+        :key="index"
+        class="example-item"
+      >
+        <q-card v-if="tip.type === 'FINANCE'" flat bordered class="q-ma-sm">
+          <q-card-section>
+            <div class="text-h6"> {{ tip.title }} </div>
+            <div class="text-subtitle2"> {{ tip.description }}</div>
+          </q-card-section>
+        </q-card>
+      </q-intersection>
+
+    </div>
+    <Pie v-if="moneyPerYear" :data="moneyPerYear" :options="options"/>
+  </q-card> -->
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, computed, watch, watchEffect } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { useStore } from 'vuex'
 
-import { showLoading } from 'src/util/Loading'
-import { LoadingStatus } from 'src/models/StatusModel'
+import DocCardTitle from 'src/components/DocCardTitle.vue'
 
-// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-// import { Pie } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  ArcElement
+} from 'chart.js'
+import { Bar, Pie } from 'vue-chartjs'
 
-// ChartJS.register(ArcElement, Tooltip, Legend)
-
-import Chart from 'chart.js/auto'
+ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Reports',
   components: {
-    // Pie
-  },
-  created () {
-    (this as any).$store.dispatch('charts/getSumMoneyPerYear', { showMessage: false, year: '2022' })
+    Pie,
+    DocCardTitle
   },
   data () {
-    onMounted(() => {
-      // showLoading(LoadingStatus.ON)
-      // console.log('disparando eventos')
-      // store.dispatch('charts/getSumMoneyPerYear', { showMessage: false, year: '2022' })
-      // .then((data) => {
-      //   moneyPerYear = data
-      //   console.log('terminei getSumMoneyPerYear')
-      // })
-      // store.dispatch('charts/getCalcExpenseByTagPerYear', { showMessage: false, year: '2022' }).then(() => { console.log('terminei getCalcExpenseByTagPerYear') })
-      // store.dispatch('charts/getCalcEarningByTagPerYear', { showMessage: false, year: '2022' }).then(() => { console.log('terminei getCalcEarningByTagPerYear') })
-    })
-    // onMounted(() => {
-    //   store.dispatch('tags/getTagsList', { showMessage: false })
-    // })
-
     const store = useStore()
-
-    const moneyPerYear = computed(() => store.getters['charts/getMoneyPerYear'])
-    // const moneyPerYearData = {
-    //   labels: moneyPerYear.value.labels,
-    //   datasets: [
-    //     {
-    //       backgroundColor: moneyPerYear.value.data.backgroundColor,
-    //       data: moneyPerYear.value.data.data
-    //     }
-    //   ]
-    // }
-    // console.log('moneyPerYear: ', moneyPerYearData)
-
-    // const expensesByYear = ref(computed(() => store.getters['charts/getExpensesPerYear']))
-    // const earningsByYear = ref(computed(() => store.getters['charts/getEarningsPerYear']))
-
-    const chartMoneyPerYear = new Chart((this as any).$refs.chart, {
-      type: 'pie',
-      data: {
-        labels: moneyPerYear.value.labels,
-        datasets: [{
-          data: moneyPerYear.value.data.data,
-          backgroundColor: moneyPerYear.value.data.backgroundColor
-        }]
-        // labels: ['January', 'February', 'March'],
-        // datasets: [{ data: [40, 20, 12] }]
-      },
-      options: {
-        responsive: true
-      }
-    })
-
-    // console.log('moneyPerYear: ', moneyPerYear)
     const options = {
-      responsive: true,
+      responsive: false,
       maintainAspectRatio: false
     }
-
-    watch(moneyPerYear, () => {
-      showLoading(LoadingStatus.OFF)
-    })
-
-    // watchEffect(async () => {
-    //   if (moneyPerYear.value === undefined) {
-    //     showLoading(LoadingStatus.OFF)
-    //   }
-    // })
 
     return {
       store,
       options,
-      // moneyPerYearData,
-      chart: ref()
-      // chartMoneyPerYear
-      // expensesByYear,
-      // earningsByYear
+      moneyPerYear: ref(null),
+      expensesByYear: ref(null),
+      earningsByYear: ref(null)
+    }
+  },
+  beforeMount () {
+    this.getSumMoneyPerYear()
+    this.getExpensesByYear()
+    this.getEarningsByYear()
+    console.log('4. passei aqui')
+  },
+  methods: {
+    getSumMoneyPerYear () {
+      console.log('1. passei aqui');
+      (this as any).$store.dispatch('charts/getSumMoneyPerYear', { showMessage: false, year: '2022' })
+    },
+    getExpensesByYear () {
+      (this as any).$store.dispatch('charts/getCalcExpenseByTagPerYear', { showMessage: false, year: '2022' })
+      console.log('2. passei aqui')
+    },
+    getEarningsByYear () {
+      (this as any).$store.dispatch('charts/getCalcEarningByTagPerYear', { showMessage: false, year: '2022' })
+      console.log('3. passei aqui')
+    }
+  },
+  computed: {
+    computedMoneyPerYear () {
+      return (this as any).$store.getters['charts/getMoneyPerYear']
+    },
+    computedExpenseByTag () {
+      return (this as any).$store.getters['charts/getExpensesPerYear']
+    },
+    computedEarningByTag () {
+      return (this as any).$store.getters['charts/getEarningsPerYear']
+    }
+  },
+  watch: {
+    computedMoneyPerYear (newValue) {
+      this.moneyPerYear = newValue
+    },
+    computedExpenseByTag (newValue) {
+      console.log(' 2.1 passei aqui: ', newValue)
+      this.expensesByYear = newValue
+    },
+    computedEarningByTag (newValue) {
+      console.log(' 3.1 passei aqui: ', newValue)
+      this.earningsByYear = newValue
     }
   }
 })
